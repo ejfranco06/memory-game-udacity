73ab24060fce5252d39d8d7d44e57cea11b491da
Emilio J. Franco <ejfranco06@gmail.com>
2018-07-05 18:59:57 -0400

    Add list of cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 /*
  * Create a list that holds all of your cards
  */
-
+const cardList = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o',
+    'fa-anchor', 'fa-anchor', 'fa-bolt', 'fa-bolt',
+    'fa-cube', 'fa-cube', 'fa-leaf', 'fa-leaf',
+    'fa-bicycle', 'fa-bicycle', 'fa-bomb', 'fa-bomb'];
 
 /*
  * Display the cards on the page
@@ -12,7 +15,7 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -24,6 +27,7 @@ function shuffle(array) {
 
     return array;
 }
+
 
 
 /*
